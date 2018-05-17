30eb19ad7f51eb1259e2c72d69389ed4977109b9
Himanshu Agrawal <hagraw@codeaurora.org>
2018-05-17 16:21:31 +0530

    msm8909go: Changing healthd version to 2.0
    Change-Id: I26044b539f75274ae05a80a43ef2486b2d67acce

--- msm8909go.mk
+++ msm8909go.mk
@@ -225,9 +225,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES_DEBUG += bootctl
 
 #Healthd packages
-PRODUCT_PACKAGES += android.hardware.health@1.0-impl \
-                    android.hardware.health@1.0-convert \
-                    android.hardware.health@1.0-service \
+PRODUCT_PACKAGES += android.hardware.health@2.0-impl \
+                    android.hardware.health@2.0-service \
                     libhealthd.msm
 #Supports verity
 PRODUCT_SUPPORTS_VERITY := true
