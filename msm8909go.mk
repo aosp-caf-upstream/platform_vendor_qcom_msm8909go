3db2fa87f8ed22edd11fd6b8f017114cce3d532a
Abhilash Kumar Subhash <asubha@codeaurora.org>
2018-12-06 18:09:46 +0530

    Revert "msm8909go: Add Vendor Compatibility Matrix information"
    This reverts commit 4a3b51093d15b315e8b369324bde3c5fc9118881.

--- msm8909go.mk
+++ msm8909go.mk
@@ -104,8 +104,6 @@ endif
 
 DEVICE_MANIFEST_FILE := device/qcom/msm8909go/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    device/qcom/common/vendor_framework_compatibility_matrix.xml
 
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
