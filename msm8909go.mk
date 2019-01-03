d665af201bc81eacd520cc384e63dd180b50167a
Mahantesh Sanakall <msanak@codeaurora.org>
2019-01-03 19:53:12 +0530

    msm8909go: Do not enable broadcastradio by default
        - Remove broadcast radio entries from manifest.xml
          and msm8909go.mk.
    
    Change-Id: I5138a5f53d04f2c486d1273975d1f4e6baeaaecc

--- msm8909go.mk
+++ msm8909go.mk
@@ -203,8 +203,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
-    android.hardware.configstore@1.0-service \
-    android.hardware.broadcastradio@1.0-impl
+    android.hardware.configstore@1.0-service
 
 ifeq ($(TARGET_HAS_LOW_RAM),true)
 TARGET_EXCLUDES_DISPLAY_PP := true
